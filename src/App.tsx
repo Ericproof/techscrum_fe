8157be164ab5d9a800fff3c0d8a60251ce2c2cfa
carzg <49139182+carzg@users.noreply.github.com>
2022-05-11 20:17:34 +1000

    html -1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import PrivacyPolicy from './pages/Privacy_Policy/Privacy_policy';
 import './App.css';
 
 function App() {
-  console.log('dssf')
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
     </Routes>
   );
 }
