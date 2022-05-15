d29c1b6c091bdb0e46b54948259818e73c48483f
shaojuanqi <qshaojuan@gmail.com>
2022-05-15 23:22:49 +1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/ Refund" element={<Refund />} />
+      <Route path="/Refund" element={<Refund />} />
     </Routes>
   );
 }
