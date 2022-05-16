135ed9770dff3de7c851ebe3db561d507ce75f2c
Ting Li <tingl0606@gmail.com>
2022-05-16 23:02:05 +1000

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import Home from "./pages/Home/Home";
+import Home from './pages/Home/Home';
 import Gdpr from './pages/Gdpr/Gdpr';
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<Home />} />
       <Route path="/Gdpr" element={<Gdpr />} />
     </Routes>
   );
