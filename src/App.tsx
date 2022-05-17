f404886ffabc976d052a6fb153f23c2bbd776eaa
Evan Lin <dunwang.lin@utas.edu.au>
2022-05-17 21:32:09 +1000

    commit message new

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import TermsOfService from './pages/TermsOfService/TermsOfService';
+import TermsOfService from './pages/TermsOfService/termsOfService';
 import './App.css';
 
 function App() {
--- src/pages/TermsOfService/TermsOfService.tsx
+++ src/pages/TermsOfService/TermsOfService.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './TermsOfService.module.scss';
+import styles from './termsOfService.module.scss';
 
 export default function TermsOfService() {
   return (
