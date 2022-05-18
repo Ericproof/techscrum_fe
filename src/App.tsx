259c8b064264cd64f7fff4808a4a8ee0c7157f73
carzg <49139182+carzg@users.noreply.github.com>
2022-05-18 16:38:06 +1000

    TEC 14 Create Privacy Policy Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import PrivacyPolicy from './pages/privacyPolicy/privacyPolicy';
+import PrivacyPolicy from './pages/privacyPolicy';
 import './App.css';
 
 function App() {
