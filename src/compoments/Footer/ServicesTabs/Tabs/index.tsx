70ee904957a1ab348186202d75262cd52efdba77
Emil <350383656@qq.com>
2022-05-18 21:23:23 +1000

    updated

--- src/compoments/Footer/ServicesTabs/Tabs/index.tsx
+++ src/compoments/Footer/ServicesTabs/Tabs/index.tsx
@@ -10,18 +10,18 @@ interface Props {
 
 export default function tab({ services }: Props) {
   return (
-      <div className={styles.service}>
-        <h1 className={styles.title}>
-          {services.title}
-          <span>+</span>
-        </h1>
-        <ul>
-          {services.contents.map((serviceItem) => (
-            <li>
-              <a href={serviceItem.href}>{serviceItem.item}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className={styles.service}>
+      <h1 className={styles.title}>
+        {services.title}
+        <span>+</span>
+      </h1>
+      <ul>
+        {services.contents.map((serviceItem) => (
+          <li>
+            <a href={serviceItem.href}>{serviceItem.item}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
