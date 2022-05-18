ab7ea4b87ca01dedc96d09aae65c64cab30df314
Emil <350383656@qq.com>
2022-05-18 23:28:41 +1000

    Tec-1 Tec-2 Bug Fixed when merge with other branch

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import Footer from '../../compoments/Footer';
 
 export default function Home() {
   return (
     <>
-    <h1 className={styles.header}>Static Routes</h1>
-    <p>Home</p>
-   </>
+      <h1 className={styles.header}>Static Routes</h1>
+      <p>Home</p>
+    </>
   );
 }
