e90eac7e802aab29094e2659ef9ee2c317f54854
Hyna <shirleymm118@gmail.com>
2022-05-19 19:11:04 +1000

    TEC-27 Setting.tsx upload

--- src/pages/Setting/Setting.tsx
+++ src/pages/Setting/Setting.tsx
@@ -3,7 +3,27 @@ import styles from './Setting.module.scss';
 
 export default function Setting() {
   return (
-  <div >
+  <div className={styles.setting}>
+    <div className={styles.setting__nav}>
+    <ol>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ol>
+    </div>
+    <div className={styles.setting__header}>
+    <h1>Details</h1>
+    <button></button>
+    </div>
+    <div className={styles.setting__container}>
+    <div className={styles.icon}>
+      <img></img>
+      <button></button>
+    </div>
+    <div className={styles.content}>
+    
+    </div>
+    </div>
 
   </div>;
   )
