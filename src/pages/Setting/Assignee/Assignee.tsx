c3defad72fb70c2c484a9db6f44e5332322b9815
Hyna <shirleymm118@gmail.com>
2022-05-22 18:05:22 +1000

    TEC-27 change assignee page tag

--- src/pages/Setting/Assignee/Assignee.tsx
+++ src/pages/Setting/Assignee/Assignee.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { grey } from '@mui/material/colors';
@@ -13,43 +13,48 @@ function Assignee() {
   const [toggle, setToggle] = useState(false);
   const [assignState, setAssignState] = useState(assignees[0]);
   return (
-    <div className={styles.assigneeContainer}>
-      {toggle ? (
-        <div className={styles.assigneeDropdownOpen}>
-          <div className={styles.assigneeMenu}>
-            <ul>
-              {assignees.map((assignee) => (
-                <li
-                  onClick={() => {
-                    setAssignState({ id: assignee.id, state: assignee.state });
-                    setToggle(false);
-                  }}
-                  key={assignee.id}
-                >
-                  <span>{assignee.state}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.assigneeInputField}>
-            <input type="Text" placeholder={assignState.state} />
-            <KeyboardArrowDownIcon
-              sx={{ color: grey[700], fontSize: 24 }}
-              onClick={() => setToggle(false)}
-            />
-          </div>
+    <div className={styles.assigneeDropdownMenu}>
+      <label htmlFor="defaultAssignee">
+        <span> Default assignee</span>
+        <div className={styles.assigneeContainer}>
+          {toggle ? (
+            <div className={styles.assigneeDropdownOpen}>
+              <div className={styles.assigneeMenu}>
+                <ul>
+                  {assignees.map((assignee) => (
+                    <li
+                      onClick={() => {
+                        setAssignState({ id: assignee.id, state: assignee.state });
+                        setToggle(false);
+                      }}
+                      key={assignee.id}
+                    >
+                      <span>{assignee.state}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.assigneeInputField}>
+                <input type="Text" placeholder={assignState.state} id="defaultAssignee" />
+                <KeyboardArrowDownIcon
+                  sx={{ color: grey[700], fontSize: 24 }}
+                  onClick={() => setToggle(false)}
+                />
+              </div>
+            </div>
+          ) : (
+            <div className={styles.assigneeDropdownClose}>
+              <div className={styles.assigneeInputClose}>
+                <span>{assignState.state}</span>
+                <KeyboardArrowDownIcon
+                  sx={{ color: grey[700], fontSize: 24 }}
+                  onClick={() => setToggle(true)}
+                />
+              </div>
+            </div>
+          )}
         </div>
-      ) : (
-        <div className={styles.assigneeDropdownClose}>
-          <div className={styles.assigneeInputClose}>
-            <span>{assignState.state}</span>
-            <KeyboardArrowDownIcon
-              sx={{ color: grey[700], fontSize: 24 }}
-              onClick={() => setToggle(true)}
-            />
-          </div>
-        </div>
-      )}
+      </label>
     </div>
   );
 }
