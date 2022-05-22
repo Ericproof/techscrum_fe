b75dcd9a99e111ee318a72db31aee80e7bcf97ed
Hyna <shirleymm118@gmail.com>
2022-05-22 18:05:50 +1000

    TEC-27 change ProjectLead page tags

--- src/pages/Setting/ProjectLead/ProjectLead.tsx
+++ src/pages/Setting/ProjectLead/ProjectLead.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from 'react';
 import userAvatar from '../../../assets/userAvatar.png';
 import styles from './ProjectLead.module.scss';
@@ -12,55 +13,56 @@ function ProjectLead() {
   const [toggle, setToggle] = useState(false);
   const [userInfo, setUserInfo] = useState(users[0]);
   return (
-    <div className={styles.leadDropdownContainer}>
-      {toggle ? (
-        <div className={styles.leadDropdownOpen}>
-          <div className={styles.leadInputField}>
-            <img className={styles.userAvatar} src={userAvatar} alt="avatar" />
-            <input type="Text" />
-            <i
-              role="button"
-              aria-label="openDropdown"
-              tabIndex={0}
-              onClick={() => setToggle(false)}
-            />
-          </div>
-          <div className={styles.leadMenu}>
-            <ul>
-              {users.map((user) => (
-                <li>
-                  <i
-                    onClick={() => {
-                      setUserInfo({ id: user.id, avatar: user.avatar, name: user.name });
-                      setToggle(false);
-                    }}
-                    key={user.id}
-                    role="button"
-                    tabIndex={0}
-                    aria-label="listDropdownItems"
-                  />
-                  <img src={user.avatar} alt="avatar" />
-                  <span>{user.name}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
+    <div className={styles.leadDropdownMenu}>
+      <label htmlFor="projectLead">
+        <span> Project lead</span>
+        <div className={styles.leadDropdownContainer}>
+          {toggle ? (
+            <div className={styles.leadDropdownOpen}>
+              <div className={styles.leadInputField}>
+                <img className={styles.userAvatar} src={userAvatar} alt="avatar" />
+                <input type="Text" />
+                <i
+                  role="button"
+                  aria-label="openDropdown"
+                  tabIndex={0}
+                  onClick={() => setToggle(false)}
+                />
+              </div>
+              <div className={styles.leadMenu}>
+                <ul>
+                  {users.map((user) => (
+                    <li
+                      onClick={() => {
+                        setUserInfo({ id: user.id, avatar: user.avatar, name: user.name });
+                        setToggle(false);
+                      }}
+                    >
+                      <img src={user.avatar} alt="avatar" />
+                      <span>{user.name}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <div className={styles.leadDropdownClose}>
+              <div
+                className={styles.leadInputClose}
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                  setToggle(true);
+                }}
+              >
+                <img src={userInfo.avatar} alt="avatar" />
+                <span>{userInfo.name}</span>
+              </div>
+            </div>
+          )}
         </div>
-      ) : (
-        <div className={styles.leadDropdownClose}>
-          <div
-            className={styles.leadInputClose}
-            role="button"
-            tabIndex={0}
-            onClick={() => {
-              setToggle(true);
-            }}
-          >
-            <img src={userInfo.avatar} alt="avatar" />
-            <span>{userInfo.name}</span>
-          </div>
-        </div>
-      )}
+      </label>
+      <p>Make sure your project lead has access to issues in the project.</p>
     </div>
   );
 }
