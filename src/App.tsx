99311c6212946f1460f87b67fe18c05aaea4e268
Hyna <shirleymm118@gmail.com>
2022-05-22 17:09:05 +1000

    TEC-27 add setting router path

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Setting from './pages/Setting/Setting';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/settings" element={<Setting />} />
     </Routes>
   );
 }
