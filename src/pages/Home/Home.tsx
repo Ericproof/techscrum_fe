b05e955f3c1e48aaecb9166d935b42b22f7611f4
Emil <350383656@qq.com>
2022-05-30 20:51:45 +1000

    TEC-1

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import styles from './Home.module.scss';
-import Header from '../../compoments/Header/Header';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <h1 className={styles.header}>Static Routes</h1>
       <p>Home</p>
     </>
