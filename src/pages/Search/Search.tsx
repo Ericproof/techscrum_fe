48dec36f6d6785f63160254adf77c8e3c45b05ce
shaojuanqi <qshaojuan@gmail.com>
2022-06-12 08:57:33 +1000

    search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,35 +1,41 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './Search.module.scss';
 
 function Search() {
-  const [search, setSearch] = useState([]);
-  const debounce = (func: any) => {
-    let timer: any;
-    return function (...args: any) {
-      const context = this;
-      if (timer) clearTimeout(timer);
-      timer = setTimeout(() => {
-        timer = null;
-        func.apply(context, args);
-      }, 500);
-    };
-  };
+  const [search, setSearch] = useState(Array<{ entityId: string; name: string }>());
+  let timer: ReturnType<typeof setTimeout>;
 
-  const handleChange = (event: any) => {
-    const { value } = event.target;
+  const handleChange = (value: string) => {
     fetch('https://www.allaboutcookies.org')
       .then((res) => res.json())
       .then((json) => setSearch(json.data.items));
+    console.log(value);
+    return true;
   };
-  const optimizedVersion = useCallback(debounce(handleChange), []);
+  const debounce = (value: string) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      handleChange(value);
+      clearTimeout(timer);
+    }, 1000);
+  };
+
+  const optimizedFn = (value: string) => {
+    debounce(value);
+  };
   return (
     <div className={styles.SearchBar}>
-      <input type="text" name="Search" placeholder="Search" onChange={optimizedVersion} />
+      <input
+        type="text"
+        name="Search"
+        placeholder="Search"
+        onChange={(e) => optimizedFn(e.target.value)}
+      />
       {setSearch.length > 0 && (
         <div className={styles.autocomplete}>
-          {search.map((notice: any) => (
-            <div key={notice.entity_id} className={styles.autocompleteItems}>
-              <span>{notice['name']}</span>
+          {search.map((notice) => (
+            <div key={notice.entityId} className={styles.autocompleteItems}>
+              <span>{notice.name}</span>
             </div>
           ))}
         </div>
