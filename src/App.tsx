d98d4f8e57d4d5325ebe6acfee7c385a362870f1
shaojuanqi <qshaojuan@gmail.com>
2022-06-13 15:07:21 +1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import PrivacyStatement from './pages/PrivacyStatement/PrivacyStatement';
 import UserPage from './pages/SettingPage/UserPage/UserPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import Access from './pages/Access/Access';
-import Search from './pages/Search/Search';
 import Project from './pages/Project/Project';
 import Board from './pages/Board';
 import './App.css';
@@ -33,7 +32,6 @@ function App() {
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="/user-page" element={<UserPage />} />
       <Route path="/access" element={<Access />} />
-      <Route path="/search" element={<Search />} />
       <Route path="/projects" element={<Project />} />
       <Route path="/board" element={<Board />} />
       <Route path="*" element={<ErrorPage />} />
