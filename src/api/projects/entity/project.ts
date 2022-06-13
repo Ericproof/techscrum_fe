808aa05bb0c23d09cbd7cc5d0521233016368328
Hyna <shirleymm118@gmail.com>
2022-06-14 00:15:55 +1000

    TEC-95 delect projects file in api

--- src/api/projects/entity/project.ts
+++
@@ -1,7 +0,0 @@
-export default interface Project{
-    id: number;
-  name: string;
-  key: string;
-  leader: { userId: number; userName: string; userIcon: string };
-  assign: { userId: string; userName: string; userIcon: string };
- }
