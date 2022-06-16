e2e1721a4c4b6c8dd40d19eb9a6031d9367e4496
Hyna <shirleymm118@gmail.com>
2022-06-17 01:08:09 +1000

    TEC-67 move ProjectEditor content and style to CreateProject tsx

--- src/pages/CreateProject/CreateProject.tsx
+++ src/pages/CreateProject/CreateProject.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Assignee from '../../components/ProjectEditor/Assignee/Assignee';
+import ChangeIcon from '../../components/ProjectEditor/ChangeIcon/ChangeIcon';
+import ChangeKey from '../../components/ProjectEditor/ChangeKey/ChangeKey';
+import ChangeName from '../../components/ProjectEditor/ChangeName/ChangeName';
+import ProjectLead from '../../components/ProjectEditor/ProjectLead/ProjectLead';
 import styles from './CreateProject.module.scss';
 
 export default function CreateProject() {
   return (
-    <div className={styles.createProjectPage}>
-      <span />
+    <div className={styles.editSection}>
+      <div className={styles.editContainer}>
+        <form>
+          <ChangeIcon />
+          <ChangeName />
+          <ChangeKey />
+          <ProjectLead />
+          <Assignee />
+          <button className={styles.saveBtn} type="submit">
+            Save
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
