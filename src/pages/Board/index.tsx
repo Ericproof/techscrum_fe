8334cf3c7e12f96a16656936be9845440213c4e2
Hyna <shirleymm118@gmail.com>
2022-06-18 02:27:07 +1000

    TEC-67 import projectHeader to board page

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import style from './index.module.scss';
 import BoardSearch from './BoardSearch/BoardSearch';
 import BoardMain from './BoardMain/BoardMain';
+import ProjectHeader from '../../components/ProjectHeader/ProjectHeader';
 
 export default function Board() {
   return (
     <div className={style.container}>
+      <ProjectHeader />
       <BoardSearch />
       <BoardMain />
     </div>
