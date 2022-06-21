1c3f811e4bdbdcf70ef91a62c0d8a4bd3a3cdfad
Hyna <shirleymm118@gmail.com>
2022-06-21 21:12:03 +1000

    TEC-108 revert another branch's changes

--- src/components/ProjectHeader/ProjectHeader.tsx
+++ src/components/ProjectHeader/ProjectHeader.tsx
@@ -30,7 +30,7 @@ export default function ProjectHeader({ projects, updateProject }: Props) {
     e.preventDefault();
     const clickStartEventFlag = (e.target as Element).className.includes('Star');
     if (!clickStartEventFlag) {
-      navigate(`/board`);
+      navigate(`/#`);
     }
   };
   const refStar = projectList.map(() => createRef<HTMLDivElement>());
@@ -93,7 +93,7 @@ export default function ProjectHeader({ projects, updateProject }: Props) {
                     <div className={styles.top}>
                       <div className={styles.recent}>RECENT</div>
                       {projectList.map((project) => (
-                        <a href="/board" onClick={handleClickEvent} key={project.id}>
+                        <a href="/projects" onClick={handleClickEvent} key={project.id}>
                           <span className={styles.iconSection}>
                             <div className={styles.iconContainer}>
                               <span className={styles.icon}>
