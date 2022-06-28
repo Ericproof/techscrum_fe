9f28eac18bfb0774542250ddfc00d689991c4430
Hyna <shirleymm118@gmail.com>
2022-06-28 21:04:14 +1000

    TEC-121 add create card button in board page & change its style

--- src/pages/Board/BoardSearch/BoardSearch.tsx
+++ src/pages/Board/BoardSearch/BoardSearch.tsx
@@ -36,69 +36,76 @@ export default function BoardSearch() {
   });
 
   return (
-    <div className={style.container}>
-      <div
-        className={
-          inputState ? `${style.inputContainer} ${style.inputContainerPlus}` : style.inputContainer
-        }
-      >
-        <input
-          type="text"
-          name="search"
-          ref={myRef}
-          placeholder={inputState ? 'Search this board' : ''}
-          onClick={() => {
-            setInputState(true);
-          }}
-        />
-        <span>
-          <img className={style.inputImg} src={search} alt="search" />
-        </span>
-      </div>
-      <fieldset>
-        <ul className={style.avatarContainer} id="myList">
-          {avatars.map((avatar) => (
-            <li
-              onClick={() => {
-                setActiveAvatarsID(avatar.id);
-                setActiveAvatars(!activeAvatars);
-              }}
-              aria-hidden="true"
-              key={avatar.id}
-            >
-              <img
-                src={avatar.url}
-                alt={avatar.name}
-                className={
-                  activeAvatarsID === avatar.id
-                    ? (activeAvatars && `${style[avatar.name]} ${style.active}`) ||
-                      style[avatar.name]
-                    : style[avatar.name]
-                }
-              />
+    <div className={style.boardSearchSection}>
+      <div className={style.container}>
+        <div
+          className={
+            inputState
+              ? `${style.inputContainer} ${style.inputContainerPlus}`
+              : style.inputContainer
+          }
+        >
+          <input
+            type="text"
+            name="search"
+            ref={myRef}
+            placeholder={inputState ? 'Search this board' : ''}
+            onClick={() => {
+              setInputState(true);
+            }}
+          />
+          <span>
+            <img className={style.inputImg} src={search} alt="search" />
+          </span>
+        </div>
+        <fieldset>
+          <ul className={style.avatarContainer} id="myList">
+            {avatars.map((avatar) => (
+              <li
+                onClick={() => {
+                  setActiveAvatarsID(avatar.id);
+                  setActiveAvatars(!activeAvatars);
+                }}
+                aria-hidden="true"
+                key={avatar.id}
+              >
+                <img
+                  src={avatar.url}
+                  alt={avatar.name}
+                  className={
+                    activeAvatarsID === avatar.id
+                      ? (activeAvatars && `${style[avatar.name]} ${style.active}`) ||
+                        style[avatar.name]
+                      : style[avatar.name]
+                  }
+                />
+              </li>
+            ))}
+            <li>
+              <button className={style.avatarButton} type="button">
+                <div>+3</div>
+              </button>
             </li>
-          ))}
-          <li>
-            <button className={style.avatarButton} type="button">
-              <div>+3</div>
-            </button>
-          </li>
-          <li className={style.roleLayout}>
-            <button type="button">
-              <span>
-                <svg width="24" height="24" viewBox="0 0 24 24" role="presentation">
-                  <g fill="currentColor" fillRule="evenodd">
-                    <rect x="18" y="5" width="2" height="6" rx="1" />
-                    <rect x="16" y="7" width="6" height="2" rx="1" />
-                    <path d="M5 14c0-1.105.902-2 2.009-2h7.982c1.11 0 2.009.894 2.009 2.006v4.44c0 3.405-12 3.405-12 0V14z" />
-                    <circle cx="11" cy="7" r="4" />
-                  </g>
-                </svg>
-              </span>
-            </button>
-          </li>
-        </ul>
-      </fieldset>
+            <li className={style.roleLayout}>
+              <button type="button">
+                <span>
+                  <svg width="24" height="24" viewBox="0 0 24 24" role="presentation">
+                    <g fill="currentColor" fillRule="evenodd">
+                      <rect x="18" y="5" width="2" height="6" rx="1" />
+                      <rect x="16" y="7" width="6" height="2" rx="1" />
+                      <path d="M5 14c0-1.105.902-2 2.009-2h7.982c1.11 0 2.009.894 2.009 2.006v4.44c0 3.405-12 3.405-12 0V14z" />
+                      <circle cx="11" cy="7" r="4" />
+                    </g>
+                  </svg>
+                </span>
+              </button>
+            </li>
+          </ul>
+        </fieldset>
+      </div>
+      <button type="button" className={style.createButton}>
+        Create card
+      </button>
     </div>
   );
 }
