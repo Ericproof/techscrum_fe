c348ce3129c940c45421b27e9bda0d0a73ab2bf5
Emil <350383656@qq.com>
2022-07-01 01:42:30 +1000

    TEC-136 board name import change

--- src/pages/Board/BoardMain/BoardMain.tsx
+++ src/pages/Board/BoardMain/BoardMain.tsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 import style from './BoardMain.module.scss';
 import EL from './img/EL-3.png';
 import universalAvatar from './img/10315.svg';
-import boardAPI from '../../../api/board/board';
+import { getBoard } from '../../../api/board/board';
 import Board from '../../../api/board/entity/board';
 import { updateTaskStatus } from '../../../api/task/task';
 
@@ -108,7 +108,7 @@ export default function BoardMain() {
     };
 
     const fetchBoardInfo = async () => {
-      const boardInfo = await boardAPI(boardId);
+      const boardInfo = await getBoard(boardId);
       fetchColumnsData(boardInfo);
     };
     fetchBoardInfo();
