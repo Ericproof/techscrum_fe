3d15052b08db3c89c55f304c48e8ac82ea05e76d
Evan Lin <dunwang.lin@utas.edu.au>
2022-07-01 16:15:14 +1000

    board search function add

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -45,6 +45,10 @@ const projects = [
   }
 ];
 export default function Board() {
+  const [inputQuery, setInputQuery] = useState<string>('');
+  // eslint-disable-next-line no-console
+  console.log(inputQuery);
+
   const projectsOrderbyDate = projects.sort((a, b) => {
     return a.lastEditTime < b.lastEditTime ? 1 : -1;
   });
@@ -70,8 +74,11 @@ export default function Board() {
       />
       <div className={style.container}>
         <HeaderNav />
-        <BoardSearch updateIsCreateNewCard={getCreateNewCardStateFromChildren} />
-        <BoardMain />
+        <BoardSearch
+          updateIsCreateNewCard={getCreateNewCardStateFromChildren}
+          setInputQuery={setInputQuery}
+        />
+        <BoardMain inputQuery={inputQuery} />
         {isCreateNewCard && (
           <CreateNewCard updateIsCreateNewCard={getCreateNewCardStateFromChildren} />
         )}
