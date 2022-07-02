0edfe2a533acee0e079531e8a22469050215c366
Emil <350383656@qq.com>
2022-07-02 18:03:03 +1000

    TEC-144 change header style by module

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,32 +7,20 @@ import LoginTabs from './LoginTabs/LoginTabs';
 
 export default function Header() {
   const [toggle, setToggle] = useState(false);
-  const [sizeToggle, setSizeToggle] = useState(false);
 
   const menuToggle = () => {
     setToggle(!toggle);
   };
 
-  React.useEffect(() => {
-    function handleResize() {
-      if (window.innerWidth < 1201) {
-        setSizeToggle(true);
-        return;
-      }
-      setSizeToggle(false);
-    }
-    window.addEventListener('resize', handleResize);
-  });
-
   return (
-    <nav className={`${styles.navNormal} ${sizeToggle && toggle ? styles.navActive : ''}`}>
+    <nav className={`${styles.navNormal} ${toggle ? styles.navActive : ''}`}>
       <div className={styles.container}>
         <Icon />
         <ServiceTabs show={toggle} />
         <div className={styles.space} />
         <LoginTabs show={toggle} />
         <div className={styles.menuBar} onMouseDown={menuToggle} role="button" tabIndex={0}>
-          {sizeToggle && toggle ? <AiOutlineClose /> : <AiOutlineMenu />}
+          {toggle ? <AiOutlineClose /> : <AiOutlineMenu />}
         </div>
       </div>
       <div className={styles.blurBackground} />
