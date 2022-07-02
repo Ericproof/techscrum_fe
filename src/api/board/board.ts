f6d338e79eec31c6ca028abfd5d167abfc13e880
kitman <kitman200220022002@gmail.com>
2022-07-02 15:22:18 +1000

    Fix getBoard

--- src/api/board/board.ts
+++ src/api/board/board.ts
@@ -1,12 +1,11 @@
+/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 import config from '../../config/config';
 
-const getBoard = async (id: string) => {
+export const getBoard = async (id: string) => {
   const path = `${config.apiAddress}/board/${id}`;
   const result = await axios.get(path).then((res) => {
     return res.data[0];
   });
   return result;
 };
-
-export default getBoard;
