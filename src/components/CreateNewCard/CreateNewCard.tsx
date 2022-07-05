2a16b6f768eb2d1d65aa96113ae2483a368bd4c3
Hyna <shirleymm118@gmail.com>
2022-07-05 19:51:10 +1000

    TEC-161 change Card name to CreateNewCard

--- src/components/CreateNewCard/CreateNewCard.tsx
+++ src/components/CreateNewCard/CreateNewCard.tsx
@@ -9,7 +9,7 @@ interface Props {
   updateIsCreateNewCard: () => void;
 }
 
-function Card({ fetchNewCard, updateIsCreateNewCard }: Props) {
+function CreateNewCard({ fetchNewCard, updateIsCreateNewCard }: Props) {
   const [description, setDescription] = useState('');
   const [title, setTitle] = useState('');
   const [hasError, setError] = useState(false);
@@ -133,4 +133,4 @@ function Card({ fetchNewCard, updateIsCreateNewCard }: Props) {
   );
 }
 
-export default Card;
+export default CreateNewCard;
