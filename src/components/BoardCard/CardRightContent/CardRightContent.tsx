064e2124f823a81478a0599f68201a6e73630be4
Hyna <shirleymm118@gmail.com>
2022-07-14 20:22:57 +1000

    TEC-174 delete useless import info

--- src/components/BoardCard/CardRightContent/CardRightContent.tsx
+++ src/components/BoardCard/CardRightContent/CardRightContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { DatePicker } from '@atlaskit/datetime-picker';
 import { TaskEntity } from '../../../api/task/entity/task';
 import { IColumnsFromBackend } from '../../../types';
