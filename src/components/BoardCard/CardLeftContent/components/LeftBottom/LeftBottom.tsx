f0710d6b102b2966ce8c0584ffc9a6a9c246c77f
Ting Li <tingl0606@gmail.com>
2022-07-21 19:57:17 +1000

    comment

--- src/components/BoardCard/CardLeftContent/components/LeftBottom/LeftBottom.tsx
+++ src/components/BoardCard/CardLeftContent/components/LeftBottom/LeftBottom.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CommentItem from './components/CommentItem/CommentItem';
 import style from './LeftBottom.module.scss';
 
 export default function LeftBottom() {
+  const [visible, setVisible] = useState(false);
+  const onFocusEventHandler = () => setVisible(true);
+  const onSaveProcessing = () => setVisible(false);
+  const onResetHandler = () => setVisible(false);
   return (
     <div className={style.container}>
       <div className={style.activity}>
@@ -12,9 +16,21 @@ export default function LeftBottom() {
           <button type="button">Comments</button>
         </div>
       </div>
-      <div className={style.commentInputField}>
-        <span role="img" className={style.avatar} />
-        <input type="text" placeholder="Add a comment..." />
+      <div>
+        <div className={style.commentInputField} onFocus={onFocusEventHandler}>
+          <span role="img" className={style.avatar} />
+          <input type="text" placeholder="Add a comment..." />
+        </div>
+        {visible && (
+          <div className={style.commentButton}>
+            <button className={style.saveButton} type="submit">
+              <span>Save</span>
+            </button>
+            <button className={style.cancelButton} type="reset">
+              <span>Cancel</span>
+            </button>
+          </div>
+        )}
       </div>
       <CommentItem />
       <CommentItem />
