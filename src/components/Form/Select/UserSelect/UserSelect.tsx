d23faeee5a6b841323f676773a3ac35ea532f8df
kitman <kitman200220022002@gmail.com>
2022-07-24 09:59:58 +1000

    fix bug

--- src/components/Form/Select/UserSelect/UserSelect.tsx
+++ src/components/Form/Select/UserSelect/UserSelect.tsx
@@ -61,7 +61,7 @@ export default function UserSelect(props: IUserSelect) {
                     >
                       <img
                         src={
-                          user.avatar ||
+                          user.avatarIcon ||
                           'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png'
                         }
                         alt="avatar"
