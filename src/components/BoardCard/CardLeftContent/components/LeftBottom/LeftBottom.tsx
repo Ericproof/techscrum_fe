437fbb5d91b0ee1ee6988702b59b155237e07435
Yiu Kitman kk <kitmanwork@gmail.com>
2022-07-23 12:03:21 +0000

    Revert "TEC-176 (pull request #180)"

--- src/components/BoardCard/CardLeftContent/components/LeftBottom/LeftBottom.tsx
+++ src/components/BoardCard/CardLeftContent/components/LeftBottom/LeftBottom.tsx
@@ -1,84 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import {
-  createComment,
-  getCommit,
-  deleteCommit,
-  updateCommit
-} from '../../../../../api/commit/commits';
-import { ICommentData, ICommentItemData } from '../../../../../types';
+import React from 'react';
 import CommentItem from './components/CommentItem/CommentItem';
 import style from './LeftBottom.module.scss';
 
-interface ILeftBottom {
-  userId?: string;
-  taskId?: string;
-  userEmail?: string;
-}
-
-export default function LeftBottom(props: ILeftBottom) {
-  const [visible, setVisible] = useState(false);
-  const [content, setContent] = useState('');
-  const { userId = '', taskId = '', userEmail = '' } = props;
-  const [comments, setComments] = useState([]);
-  const [saveState, setSaveState] = useState(false);
-  const [deleteState, setDeleteState] = useState(false);
-  const [updateState, setUpdateState] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const onFocusEventHandler = () => setVisible(true);
-
-  const fetchCommentsData = () => {
-    async function fetchData() {
-      await getCommit().then((data: ICommentData) => {
-        const result = data.data.reverse();
-        setComments(result);
-      });
-    }
-    fetchData();
-  };
-
-  const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setContent(e.target.value);
-  };
-
-  const onClickDelete = async (id: string) => {
-    await deleteCommit(id);
-    setDeleteState(true);
-  };
-
-  const onClickUpdate = async (id: string, commentContent: string) => {
-    await updateCommit(id, commentContent);
-    setUpdateState(true);
-  };
-
-  const onClickSave = async () => {
-    await createComment({ taskId, senderId: userId, content });
-    setSaveState(true);
-    setVisible(false);
-    setContent('');
-  };
-
-  useEffect(() => {
-    if (isLoading) {
-      fetchCommentsData();
-      setIsLoading(false);
-    }
-    if (saveState) {
-      fetchCommentsData();
-      setSaveState(false);
-    }
-
-    if (deleteState) {
-      fetchCommentsData();
-      setDeleteState(false);
-    }
-    if (updateState) {
-      fetchCommentsData();
-      setUpdateState(false);
-    }
-  }, [isLoading, saveState, deleteState, updateState]);
-
-  const onResetHandler = () => setVisible(false);
+export default function LeftBottom() {
   return (
     <div className={style.container}>
       <div className={style.activity}>
@@ -88,50 +12,12 @@ export default function LeftBottom(props: ILeftBottom) {
           <button type="button">Comments</button>
         </div>
       </div>
-      <div>
-        <div className={style.commentInputField} onFocus={onFocusEventHandler}>
-          <span role="img" className={style.avatar} />
-          <input
-            value={content}
-            onChange={onChangeContent}
-            type="text"
-            placeholder="Add a comment..."
-          />
-        </div>
-        {visible && (
-          <div className={style.commentButton}>
-            <button className={style.saveButton} type="button" onClick={onClickSave}>
-              <span>Save</span>
-            </button>
-            <button className={style.cancelButton} type="reset" onClick={onResetHandler}>
-              <span>Cancel</span>
-            </button>
-          </div>
-        )}
+      <div className={style.commentInputField}>
+        <span role="img" className={style.avatar} />
+        <input type="text" placeholder="Add a comment..." />
       </div>
-      {comments.map((item: ICommentItemData) => {
-        if (item.taskId === taskId) {
-          return (
-            <CommentItem
-              key={item.id}
-              content={item.content}
-              id={item.id}
-              senderId={item.senderId}
-              updatedAt={item.updatedAt}
-              onClickDelete={onClickDelete}
-              onClickUpdate={onClickUpdate}
-              userEmail={userEmail}
-            />
-          );
-        }
-        return null;
-      })}
+      <CommentItem />
+      <CommentItem />
     </div>
   );
 }
-
-LeftBottom.defaultProps = {
-  taskId: undefined,
-  userId: undefined,
-  userEmail: undefined
-};
