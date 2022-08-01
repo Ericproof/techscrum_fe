a21f2b7266c3578326a3276dbb6fe9abf46a55a4
Evan Lin <dunwang.lin@utas.edu.au>
2022-08-01 20:31:44 +1000

    add to favorite function

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -19,6 +19,7 @@ alphaApi.interceptors.request.use((config: any) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
   config.headers['Content-Type'] = 'application/json';
+
   return config;
 });
 
