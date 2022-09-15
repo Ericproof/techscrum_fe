bb0f085a4f9732f49a6c00cd421e55fcb1d737e6
davidguo0011 <davidg9911mail@gmail.com>
2022-09-15 18:25:47 +1200

    created FAQ page by David

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ import UnauthorizePage from './pages/UnauthorizePage/UnauthorizePage';
 import { RolesProvider } from './context/UserPermissionProvider';
 import { TaskTypesProvider } from './context/TaskTypeProvider';
 import ContactPage from './pages/ContactPage/ContactPage';
+import FAQPage from './pages/FAQpage/FAQPage';
 
 function App() {
   const shouldShowRegister =
@@ -44,6 +45,7 @@ function App() {
           <TaskTypesProvider>
             <Routes>
               {shouldShowRegister && <Route path="/register" element={<RegisterPage />} />}
+              <Route path="/FAQ" element={<FAQPage />} />
               <Route path="/verify" element={<VerifyPage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/" element={<HomePage />} />
---
+++ src/pages/FAQpage/components/FAQbutton/FAQbutton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './FAQbutton.module.scss';
+
+export default function FAQbutton() {
+  return <button className={styles.btn}>View All</button>;
+}
