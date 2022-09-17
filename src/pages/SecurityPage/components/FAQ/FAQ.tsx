2b4011aecc79070355fea724b5ce87ccd74248af
Summy <s4594789@student.uq.edu.au>
2022-09-17 21:13:22 +1000

    modify resusable contents

--- src/pages/SecurityPage/components/FAQ/FAQ.tsx
+++ src/pages/SecurityPage/components/FAQ/FAQ.tsx
@@ -1,75 +1,63 @@
-// eslint-disable no-console, no-control-regex
+/* eslint-disable no-console */
 
-import React from 'react';
-import { FaRegPlusSquare } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaRegPlusSquare, FaRegMinusSquare } from 'react-icons/fa';
+import { icons } from 'react-icons/lib';
 import styles from './FAQ.module.scss';
+import FAQComponent from './FAQComponent/FAQComponent';
 
 export default function Certifications() {
+  const [display, setDisplay] = useState(false);
+  let icon;
+  if (display) {
+    icon = <FaRegMinusSquare className={styles.PlusIcon} />;
+  } else {
+    icon = <FaRegPlusSquare className={styles.PlusIcon} />;
+  }
+
+  function toggleDisplay() {
+    if (display) {
+      setDisplay(false);
+    } else {
+      setDisplay(true);
+    }
+  }
+
+  const objs = [
+    {
+      title: 'Where are your servers located',
+      content:
+        'Our servers are with Amazon Web Services AWS and are hosted within the US and the EU. For more information on AWS, click here.'
+    },
+    {
+      title: 'Who has access to the servers?',
+      content:
+        'Only certain members of our team have authorization to access the servers. We never breach customer confidentiality.'
+    },
+    {
+      title: 'What about physical security?',
+      content:
+        'AWS infrastructure is housed in Amazon-controlled data centers throughout the world. Only those within Amazon who have a legitimate business need to have such information know the actual location of these data centers.'
+    },
+    {
+      title: 'Where can I find your uptime report?',
+      content: 'You can see the latest uptime report https://status.teamwork.com/.'
+    },
+    {
+      title: 'Where do I report a security concern?',
+      content: 'You can report a security concern by emailing us at security@teamwork.com.'
+    }
+  ];
+
+  const faqComponent = objs.map((obj) => {
+    return <FAQComponent title={obj.title} content={obj.content} />;
+  });
+
   return (
     <div className={styles.FAQRoot}>
       <h3>Frequently asked questions</h3>
       <div className={styles.FAQDetailesContainer}>
-        <div
-          className={styles.FAQDetailes}
-          role="button"
-          tabIndex={0}
-          onClick={() => {}}
-          onKeyDown={() => {}}
-        >
-          <div className={styles.Qcontainer}>
-            <h4>Where are your servers located?</h4>
-          </div>
-          <div className={styles.IconContainer}>
-            <FaRegPlusSquare className={styles.PlusIcon} />
-          </div>
-        </div>
-
-        <hr className={styles.breaker} />
-
-        <div className={styles.FAQDetailes}>
-          <div className={styles.Qcontainer}>
-            <h4>Who has access to the servers?</h4>
-          </div>
-          <div className={styles.IconContainer}>
-            <FaRegPlusSquare className={styles.PlusIcon} />
-          </div>
-        </div>
-
-        <hr className={styles.breaker} />
-
-        <div className={styles.FAQDetailes}>
-          <div className={styles.Qcontainer}>
-            <h4>What about physical security?</h4>
-          </div>
-          <div className={styles.IconContainer}>
-            <FaRegPlusSquare className={styles.PlusIcon} />
-          </div>
-        </div>
-
-        <hr className={styles.breaker} />
-
-        <div className={styles.FAQDetailes}>
-          <div className={styles.Qcontainer}>
-            <h4>Where can I find your uptime report?</h4>
-          </div>
-          <div className={styles.IconContainer}>
-            <FaRegPlusSquare className={styles.PlusIcon} />
-          </div>
-        </div>
-
-        <hr className={styles.breaker} />
-
-        <div className={styles.FAQDetailes}>
-          <div className={styles.Qcontainer}>
-            <h4>Where do I report a security concern?</h4>
-          </div>
-          <div className={styles.IconContainer}>
-            <FaRegPlusSquare className={styles.PlusIcon} />
-          </div>
-        </div>
-
-        <hr className={styles.breaker} />
-
+        {faqComponent}
         <div className={styles.FAQInfoContainer}>
           <div className={styles.desc}>
             <p>
---
+++ src/pages/SecurityPage/components/FAQ/FAQComponent/FAQComponent.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-console */
+
+import React, { useState } from 'react';
+import { FaRegPlusSquare, FaRegMinusSquare } from 'react-icons/fa';
+import styles from './FAQComponent.module.scss';
+
+interface Imember {
+  title: string;
+  content: string;
+}
+
+export default function FAQComponent(props: Imember) {
+  const { title, content } = props;
+  const [display, setDisplay] = useState(false);
+  let icon;
+  if (display) {
+    icon = <FaRegMinusSquare className={styles.PlusIcon} />;
+  } else {
+    icon = <FaRegPlusSquare className={styles.PlusIcon} />;
+  }
+
+  function toggleDisplay() {
+    if (display) {
+      setDisplay(false);
+    } else {
+      setDisplay(true);
+    }
+  }
+
+  return (
+    <>
+      <div
+        className={styles.FAQDetailes}
+        role="button"
+        tabIndex={0}
+        onClick={() => {
+          toggleDisplay();
+        }}
+        onKeyDown={() => {}}
+      >
+        <div className={styles.Qcontainer}>
+          <h4>{title}</h4>
+        </div>
+        <div className={styles.IconContainer}>{icon}</div>
+      </div>
+      <hr className={styles.breaker} />
+      <div className={display ? styles.expand : styles.hide}>
+        <p>{content}</p>
+      </div>
+    </>
+  );
+}
