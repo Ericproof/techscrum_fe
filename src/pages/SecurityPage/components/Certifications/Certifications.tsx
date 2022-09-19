44ae71e2d6929c7bdadb7ffc47a8814bb7916521
天羽よつは <amouyotsuha@amouyotsuhas-MacBook-Air.local>
2022-09-19 19:54:42 +1000

    class name fix

--- src/pages/SecurityPage/components/Certifications/Certifications.tsx
+++ src/pages/SecurityPage/components/Certifications/Certifications.tsx
@@ -11,19 +11,26 @@ export default function Certifications() {
           us. Learn more about our commitment to best-in-class information security standards below.
         </p>
         <p className={styles.DescSpan}>
-          <a href="https://www.teamwork.com/security/iso/">ISO/IEC 27001:2013</a>
-          <a href="https://www.teamwork.com/security/soc-2-certification/">
+          <a className={styles.isoLink} href="https://www.teamwork.com/security/iso/">
+            ISO/IEC 27001:2013
+          </a>
+          <a
+            className={styles.isoLink}
+            href="https://www.teamwork.com/security/soc-2-certification/"
+          >
             SOC 2 Type 2 certified
           </a>
         </p>
-        <button>Learn More</button>
+        <button className={styles.learnMoreButton}>Learn More</button>
       </div>
-      <div className={styles.CertImg}>
+      <div className={styles.CertImgs}>
         <img
+          className={styles.CertImg}
           src="https://website-assets.teamwork.com/offload/app/uploads/2020/07/22164825/soc-logo.png"
           alt="SOC 2 Type 1 certified"
         />
         <img
+          className={styles.CertImg}
           src="https://website-assets.teamwork.com/offload/app/uploads/2020/07/22164823/ISO27001-logo.png"
           alt="ISO 27001 Certification"
         />
