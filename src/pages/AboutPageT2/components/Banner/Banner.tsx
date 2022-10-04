c270c82de825feec66a0d837c9d7074da703eeee
kitman <kitman200220022002@gmail.com>
2022-10-04 17:21:26 +1100

    tec-316 mod about page

--- src/pages/AboutPageT2/components/Banner/Banner.tsx
+++ src/pages/AboutPageT2/components/Banner/Banner.tsx
@@ -4,12 +4,12 @@ import styles from './Banner.module.scss';
 export default function Banner() {
   return (
     <section className={styles.container}>
-      <h1>Hello! We are Teamwork</h1>
-      <p>
+      <h1 className={styles.header}>Hello! We are Teamwork</h1>
+      <p className={styles.text}>
         Our mission is to make teams that deliver client work <strong>efficient, organized,</strong>{' '}
         profitable and <strong>happy.</strong>
       </p>
-      <hr />
+      <hr className={styles.line} />
     </section>
   );
 }
--- src/pages/AboutPageT2/components/OurValuesCards/CardsOfValues.tsx
+++ src/pages/AboutPageT2/components/OurValuesCards/CardsOfValues.tsx
@@ -8,10 +8,40 @@ export default function CardsOfValues() {
       imageLink:
         'https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131716/value-open.svg',
       altInfo: 'girl eat apple'
+    },
+    {
+      description: 'We’re open, trustworthy and down to earth',
+      imageLink:
+        'https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131716/value-open.svg',
+      altInfo: 'girl eat apple'
+    },
+    {
+      description: 'We’re open, trustworthy and down to earth',
+      imageLink:
+        'https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131716/value-open.svg',
+      altInfo: 'girl eat apple'
+    },
+    {
+      description: 'We’re open, trustworthy and down to earth',
+      imageLink:
+        'https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131716/value-open.svg',
+      altInfo: 'girl eat apple'
+    },
+    {
+      description: 'We’re open, trustworthy and down to earth',
+      imageLink:
+        'https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131716/value-open.svg',
+      altInfo: 'girl eat apple'
+    },
+    {
+      description: 'We’re open, trustworthy and down to earth',
+      imageLink:
+        'https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131716/value-open.svg',
+      altInfo: 'girl eat apple'
     }
   ];
   return (
-    <section className={styles.container}>
+    <section className={[styles.container, styles.cardOfValuesContainer].join(' ')}>
       <h1>Our Values</h1>
       <p>
         No matter what size our business grows to, these are the values that guide our decision
@@ -26,47 +56,6 @@ export default function CardsOfValues() {
           </div>
         );
       })}
-
-      <div className={styles.card}>
-        <span className={styles.cardDescription}>We’re passionate about what we do</span>
-        <img
-          className={styles.cardImg}
-          src="https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131716/value-passionate.svg"
-          alt="boy use phone"
-        />
-      </div>
-      <div className={styles.card}>
-        <span className={styles.cardDescription}>We put our customers first</span>
-        <img
-          className={styles.cardImg}
-          src="https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131716/value-customers.svg"
-          alt="girl with medal"
-        />
-      </div>
-      <div className={styles.card}>
-        <span className={styles.cardDescription}>We strive for excellence</span>
-        <img
-          className={styles.cardImg}
-          src="https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131717/value-strive-excellence.svg"
-          alt="medal"
-        />
-      </div>
-      <div className={styles.card}>
-        <span className={styles.cardDescription}>We’re results driven</span>
-        <img
-          className={styles.cardImg}
-          src="https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131717/value-results-driven.svg"
-          alt="bar chart"
-        />
-      </div>
-      <div className={styles.card}>
-        <span className={styles.cardDescription}>We value teamwork</span>
-        <img
-          className={styles.cardImg}
-          src="https://website-assets.teamwork.com/offload/app/uploads/2019/02/22131717/value-teamwork.svg"
-          alt="shake hands"
-        />
-      </div>
     </section>
   );
 }
