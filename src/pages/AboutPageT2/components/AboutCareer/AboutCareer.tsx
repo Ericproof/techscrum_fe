0fa176af1ee7db1eaa67ad7812987f8d28eb009d
kitman20022002 <kitman200220022002@gmail.com>
2022-10-06 15:40:18 +1100

    tec-316 completed about page top 3 section

--- src/pages/AboutPageT2/components/AboutCareer/AboutCareer.tsx
+++ src/pages/AboutPageT2/components/AboutCareer/AboutCareer.tsx
@@ -4,20 +4,23 @@ import styles from './AboutCareer.module.scss';
 export default function AboutCareer() {
   return (
     <section className={styles.container}>
-      <div className={styles.leftPart}>
-        <h1>Come join us!</h1>
+      <div className={styles.sectionContiner}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.header}>Come join us!</h1>
+          <p className={styles.text}>
+            We’re always on the lookout for exceptional talent so we’d love to hear from you.
+          </p>
 
-        <p>We’re always on the lookout for exceptional talent so we’d love to hear from you.</p>
-
-        <div className={styles.positionLink}>
-          <a href="none">See our open positions</a>
+          <div className={styles.positionLink}>
+            <a href="none">See our open positions</a>
+          </div>
         </div>
+        <img
+          className={styles.img}
+          src="https://www.teamwork.com/_nuxt/assets/images/about/teamwork-work-here.webp"
+          alt="Images of working environment"
+        />
       </div>
-      <img
-        className={styles.rightPart}
-        src="https://www.teamwork.com/_nuxt/assets/images/about/teamwork-work-here.webp"
-        alt="Images of working environment"
-      />
     </section>
   );
 }
