8eeb205029a29fb27be9d537e9daab29703e62c8
FelixMJL <felix_liu03@hotmail.com>
2022-10-07 15:16:19 +1100

    TEC-316 about page responsive

--- src/pages/AboutPageT2/AboutPageT2.tsx
+++ src/pages/AboutPageT2/AboutPageT2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Banner from './components/Banner/Banner';
+import Greeting from './components/Banner/Banner';
 import styles from './AboutPageT2.module.scss';
 import CardsOfValues from './components/OurValuesCards/CardsOfValues';
 import AboutCareer from './components/AboutCareer/AboutCareer';
@@ -8,16 +8,12 @@ import GetStarted from './components/GetStarted/GetStarted';
 export default function AboutPageT2() {
   return (
     <div className={styles.container}>
-      <div className={styles.greeting}>
-        <Banner />
+      <div className="greeting">
+        <Greeting />
       </div>
 
       <div className="introduction-clip">
-        <img
-          src="https://embed-ssl.wistia.com/deliveries/c3648fe3ea850ffce14b1b3d78c6a517.webp?image_crop_resized=1920x1080"
-          alt="intro"
-          className={styles.introImg}
-        />
+        <p>introduction-clip</p>
       </div>
 
       <div>
@@ -28,7 +24,7 @@ export default function AboutPageT2() {
         <AboutCareer />
       </div>
 
-      <div className="background-white">
+      <div>
         <GetStarted />
       </div>
     </div>
--- src/pages/AboutPageT2/components/GetStarted/GetStarted.tsx
+++ src/pages/AboutPageT2/components/GetStarted/GetStarted.tsx
@@ -4,22 +4,24 @@ import styles from './GetStarted.module.scss';
 export default function GetStarted() {
   return (
     <section className={styles.getStartedContainer}>
-      <div className={styles.getStartedIntroduction}>
-        <h1>Get started with Teamwork</h1>
-        <p>
-          Start working together beautifully. See how Teamwork can help your team with our 30-day
-          free trial.
-        </p>
-      </div>
-      <div className={styles.getStartedInteractionLinks}>
-        <div className={styles.getStartedlinksBtn}>
-          <a href="none">Try Teamwork for free</a>
+      <div className={styles.getStartedContentContainer}>
+        <div className={styles.getStartedIntroduction}>
+          <h1>Get started with Teamwork</h1>
+          <p>
+            Start working together beautifully. See how Teamwork can help your team with our 30-day
+            free trial.
+          </p>
         </div>
-        <div className={styles.getStartedlinksBtn}>
-          <a href="none">Join a webinar</a>
-        </div>
-        <div className={styles.getStartedlinksBtn}>
-          <a href="none">Get in touch</a>
+        <div className={styles.getStartedInteractionLinks}>
+          <div className={styles.getStartedlinksBtn}>
+            <a href="none">Try Teamwork for free</a>
+          </div>
+          <div className={styles.getStartedlinksBtn}>
+            <a href="none">Join a webinar</a>
+          </div>
+          <div className={styles.getStartedlinksBtn}>
+            <a href="none">Get in touch</a>
+          </div>
         </div>
       </div>
     </section>
