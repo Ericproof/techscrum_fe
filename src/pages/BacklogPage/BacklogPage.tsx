7e21398af3798a09701509cc371e19175f3bf3b4
Joe Zhou <zhouhaohangau@gmail.com>
2022-10-14 02:28:49 +1100

    allow task list to scroll if exceeded view port height

--- src/pages/BacklogPage/BacklogPage.tsx
+++ src/pages/BacklogPage/BacklogPage.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import DashboardLayout from '../../components/DashboardLayout/DashboardLayout';
 import BacklogSection from './BacklogSection/BacklogSection';
 import SprintSection from './SprintSection/SprintSection';
+import styles from './BacklogPage.module.scss';
 
 export default function BacklogPage() {
   // WIP fetch data from backend and pass donw
 
   return (
     <DashboardLayout>
-      <div>
-        <h1>Backlog</h1>
+      <div className={styles.container}>
+        <div>
+          <h1>Backlog</h1>
+        </div>
+        <div className={styles.scrollContainer}>
+          <SprintSection />
+          <BacklogSection />
+        </div>
       </div>
-      <SprintSection />
-      <BacklogSection />
     </DashboardLayout>
   );
 }
