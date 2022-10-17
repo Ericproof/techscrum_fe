634bd8468e08eff66430ec769431a6893697689e
FelixMJL <felix_liu03@hotmail.com>
2022-10-17 15:57:26 +1100

    updated styles

--- src/pages/AboutPageT2/components/AboutCareer/AboutCareer.tsx
+++ src/pages/AboutPageT2/components/AboutCareer/AboutCareer.tsx
@@ -10,11 +10,9 @@ export default function AboutCareer() {
           <p className={styles.text}>
             We’re always on the lookout for exceptional talent so we’d love to hear from you.
           </p>
-          {/* <div className={styles.positionLink}> */}
           <a className={styles.positionLink} href="none">
             See our open positions
           </a>
-          {/* </div> */}
         </div>
         <img
           className={styles.img}
