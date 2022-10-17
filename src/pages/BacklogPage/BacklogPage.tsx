2a402bc8c002bed1398f920cb4dd68c86c314a3c
davidguo0011 <davidg9911mail@gmail.com>
2022-10-18 02:54:12 +1000

    removed unused imports and comments

--- src/pages/BacklogPage/BacklogPage.tsx
+++ src/pages/BacklogPage/BacklogPage.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { getBacklogData } from '../../api/backlog/backlog';
+import React, { useState } from 'react';
 import BacklogSection from './BacklogSection/BacklogSection';
 import SprintSection from './SprintSection/SprintSection';
 import styles from './BacklogPage.module.scss';
@@ -7,24 +6,15 @@ import styles from './BacklogPage.module.scss';
 export default function BacklogPage() {
   // WIP need to communicate with backend
 
-  const [backlogData, setBacklogData] = useState(true);
-  // useEffect(() => {
-  //   getBacklogData().then((response) => {
-  //     setBacklogData(response);
-  //   });
-  // }, []);
-
   return (
     <div className={styles.container}>
       <div>
         <h1>Backlog</h1>
       </div>
-      {backlogData && (
-        <div className={styles.scrollContainer}>
-          <SprintSection />
-          <BacklogSection />
-        </div>
-      )}
+      <div className={styles.scrollContainer}>
+        <SprintSection />
+        <BacklogSection />
+      </div>
     </div>
   );
 }
