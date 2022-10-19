d9e34b161df9887cf62a1973f5d16f2e8bbb9277
davidguo0011 <davidg9911mail@gmail.com>
2022-10-20 02:03:00 +1000

    removed unused imports

--- src/components/DailyScrum/DailyScrumTicket/DailyScrumTicket.tsx
+++ src/components/DailyScrum/DailyScrumTicket/DailyScrumTicket.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './DailyScrumTicket.module.scss';
 import RadioInput from '../../ReusableElement/RadioInput/RadioInput';
 
