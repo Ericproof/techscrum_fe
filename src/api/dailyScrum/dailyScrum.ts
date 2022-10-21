105dd889ed522eeb649d86f3a3f3bb607d147de2
davidguo0011 <davidg9911mail@gmail.com>
2022-10-21 15:12:56 +1000

    fixed naming convention dailyscrum api

--- src/api/dailyScrum/dailyScrum.ts
+++ src/api/dailyScrum/dailyScrum.ts
@@ -1,22 +1,22 @@
 import axios from 'axios';
 import config from '../../config/config';
 
-export const getAllDailyScrums = async (projectId: string) => {
+export const getDailyScrums = async (projectId: string) => {
   return axios.get(`${config.apiAddress}/projects/${projectId}/dailyScrums`);
 };
 
-export const getOneDailyScrum = async (projectId: string, id: string) => {
+export const getDailyScrum = async (projectId: string, id: string) => {
   return axios.get(`${config.apiAddress}/projects/${projectId}/dailyScrums/${id}`);
 };
 
-export const createOneDailyScrum = async (projectId: string, data: any) => {
+export const createDailyScrum = async (projectId: string, data: any) => {
   return axios.post(`${config.apiAddress}/projects/${projectId}/dailyScrums`, data);
 };
 
-export const updateOneDailyScrum = async (projectId: string, id: string, data: any) => {
+export const updateDailyScrum = async (projectId: string, id: string, data: any) => {
   return axios.put(`${config.apiAddress}/projects/${projectId}/dailyScrums/${id}`, data);
 };
 
-export const deleteOneDailyScrum = async (projectId: string, id: string) => {
+export const deleteDailyScrum = async (projectId: string, id: string) => {
   return axios.delete(`${config.apiAddress}/projects/${projectId}/dailyScrums/${id}`);
 };
