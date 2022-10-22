5303343c1b29d9b5d99ea6090e492be5695b0797
davidguo0011 <davidg9911mail@gmail.com>
2022-10-22 13:25:10 +1000

    dailyscrum button UI fixed

--- src/components/BoardNavigation/NavMain/NavMain.tsx
+++ src/components/BoardNavigation/NavMain/NavMain.tsx
@@ -4,6 +4,7 @@ import { VscNewFile } from 'react-icons/vsc';
 import { NavLink, useParams } from 'react-router-dom';
 import { BsFillPeopleFill } from 'react-icons/bs';
 import { FiSettings } from 'react-icons/fi';
+import { FaDailymotion } from 'react-icons/fa';
 import { IProjectData, IShortcutData } from '../../../types';
 import checkAccess from '../../../utils/helpers';
 import Shortcut from '../../AddShortcut/Shortcut';
@@ -232,7 +233,8 @@ export default function NavMain(props: IPropsNavMain) {
           }}
           className={styles.dailyScrumBtn}
         >
-          Daily scrum
+          <FaDailymotion className={styles.dailyScrumIcon} />
+          <p>Daily scrum</p>
         </button>
         {showDailyScrum && (
           <DailyScrum
