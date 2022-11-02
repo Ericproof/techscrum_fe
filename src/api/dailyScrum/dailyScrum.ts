00c42e5bd414ed48bfb7102d27275de2a4137e1a
Summy <s4594789@student.uq.edu.au>
2022-11-02 22:28:24 +1000

    add dailyScrum backend

--- src/api/dailyScrum/dailyScrum.ts
+++ src/api/dailyScrum/dailyScrum.ts
@@ -5,18 +5,26 @@ export const getDailyScrums = async (projectId: string) => {
   return axios.get(`${config.apiAddress}/projects/${projectId}/dailyScrums`);
 };
 
-export const getDailyScrum = async (projectId: string, id: string) => {
-  return axios.get(`${config.apiAddress}/projects/${projectId}/dailyScrums/${id}`);
+export const getDailyScrum = async (projectId: string, userId: string) => {
+  return axios.get(`${config.apiAddress}/projects/${projectId}/dailyScrums/${userId}`);
 };
 
 export const createDailyScrum = async (projectId: string, data: any) => {
   return axios.post(`${config.apiAddress}/projects/${projectId}/dailyScrums`, data);
 };
 
-export const updateDailyScrum = async (projectId: string, id: string, data: any) => {
-  return axios.put(`${config.apiAddress}/projects/${projectId}/dailyScrums/${id}`, data);
+export const updateDailyScrum = async (
+  projectId: string,
+  userId: string,
+  taskId: string,
+  data: any
+) => {
+  return axios.patch(
+    `${config.apiAddress}/projects/${projectId}/dailyScrums/${userId}/${taskId}`,
+    data
+  );
 };
 
-export const deleteDailyScrum = async (projectId: string, id: string) => {
-  return axios.delete(`${config.apiAddress}/projects/${projectId}/dailyScrums/${id}`);
+export const deleteDailyScrum = async (projectId: string, taskId: string) => {
+  return axios.delete(`${config.apiAddress}/projects/${projectId}/dailyScrums/${taskId}`);
 };
