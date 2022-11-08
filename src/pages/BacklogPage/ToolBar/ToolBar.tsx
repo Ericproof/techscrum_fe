557d2d445462b420c636abcfd4d678c31214e5bf
davidguo0011 <davidg9911mail@gmail.com>
2022-11-08 21:23:58 +1000

    done

--- src/pages/BacklogPage/ToolBar/ToolBar.tsx
+++ src/pages/BacklogPage/ToolBar/ToolBar.tsx
@@ -3,15 +3,25 @@ import styles from './ToolBar.module.scss';
 import Avartar from '../../../assets/userAvatar.png';
 import IconButton from '../../../components/Button/IconButton/IconButton';
 import StatusBtn from '../StatusBtn/StatusBtn';
+import PriorityBtn from '../PriorityBtn/PriorityBtn';
 
 interface IToolBar {
   status: string;
   onClickChangeStatus: (id: string, status: string) => void;
   taskId: string;
+  priority: string;
+  onClickChangePriority: (id: string, priority: string) => void;
 }
-export default function ToolBar({ status, onClickChangeStatus, taskId }: IToolBar) {
+export default function ToolBar({
+  status,
+  onClickChangeStatus,
+  taskId,
+  onClickChangePriority,
+  priority
+}: IToolBar) {
   return (
     <div className={styles.toolbar}>
+      <PriorityBtn id={taskId} onClickChangePriority={onClickChangePriority} priority={priority} />
       <StatusBtn status={status} onClickChangeStatus={onClickChangeStatus} taskId={taskId} />
       <IconButton
         overrideStyle={styles.assignee}
