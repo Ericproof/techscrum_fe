f11d752fc18b6f9b685771c96a8d957583c1d55c
davidguo0011 <davidg9911mail@gmail.com>
2022-11-08 21:24:26 +1000

    remove comments

--- cypress/e2e/11-backlog-page/backlogPage.cy.js
+++ cypress/e2e/11-backlog-page/backlogPage.cy.js
@@ -19,53 +19,53 @@ describe('Backlog page', () => {
     cy.wait('@fetch-backlog');
   });
 
-  // it('Test backlog page show tasks', () => {
-  //   cy.get('[data-testid="task-6350dbbca5c71eda4bcf78aa"]').contains('asdf');
-  //   cy.get('[data-testid="task-6350dcfa560c73ef4f32e2a6"]').contains('daf');
-  //   cy.get('[data-testid="task-6350dfbbca131ce6228f3e59"]').contains('daffdd');
-  // });
+  it('Test backlog page show tasks', () => {
+    cy.get('[data-testid="task-6350dbbca5c71eda4bcf78aa"]').contains('asdf');
+    cy.get('[data-testid="task-6350dcfa560c73ef4f32e2a6"]').contains('daf');
+    cy.get('[data-testid="task-6350dfbbca131ce6228f3e59"]').contains('daffdd');
+  });
 
-  // it('Test create task', () => {
-  //   cy.intercept('POST', '**/tasks', backlogDataAddTask).as('create-issue');
-  //   cy.intercept('GET', '**/projects/*/backlogs', backlogDataAddTask).as('fetch-backlog-2');
-  //   cy.get('[data-testid="create-issue"]').click();
-  //   cy.get('[data-testid="create-issue-input"]').type('eat lunch');
-  //   cy.get('input').type('{enter}');
-  //   cy.wait('@create-issue');
-  //   cy.wait('@fetch-backlog-2');
-  //   cy.get('[data-testid="task-6350dbbca5c71eda4bcf78aa3"]').contains('eat lunch');
-  // });
+  it('Test create task', () => {
+    cy.intercept('POST', '**/tasks', backlogDataAddTask).as('create-issue');
+    cy.intercept('GET', '**/projects/*/backlogs', backlogDataAddTask).as('fetch-backlog-2');
+    cy.get('[data-testid="create-issue"]').click();
+    cy.get('[data-testid="create-issue-input"]').type('eat lunch');
+    cy.get('input').type('{enter}');
+    cy.wait('@create-issue');
+    cy.wait('@fetch-backlog-2');
+    cy.get('[data-testid="task-6350dbbca5c71eda4bcf78aa3"]').contains('eat lunch');
+  });
 
-  // it('Test delete task', () => {
-  //   //create task
-  //   cy.intercept('POST', '**/tasks', backlogDataAddTask).as('create-issue');
-  //   cy.intercept('GET', '**/projects/*/backlogs', backlogDataAddTask).as('fetch-backlog-2');
-  //   cy.get('[data-testid="create-issue"]').click();
-  //   cy.get('[data-testid="create-issue-input"]').type('eat lunch');
-  //   cy.get('input').type('{enter}');
-  //   cy.wait('@create-issue');
-  //   cy.wait('@fetch-backlog-2');
-  //   cy.get('[data-testid="task-6350dbbca5c71eda4bcf78aa3"]').contains('eat lunch');
-  //   //delete task
-  //   cy.intercept('DELETE', '**/tasks/*', backlogData).as('delete-issue');
-  //   cy.intercept('GET', '**/projects/*/backlogs', backlogData).as('fetch-backlog');
-  //   cy.get('[data-testid="task-6350dbbca5c71eda4bcf78aa3"]').trigger('mouseover');
-  //   cy.get('[data-testid="hover-show-option-btn-6350dbbca5c71eda4bcf78aa3"]').click();
-  //   cy.get('[data-testid="delete-task-6350dbbca5c71eda4bcf78aa3"]').click();
-  //   cy.wait('@delete-issue');
-  //   cy.wait('@fetch-backlog');
-  // });
+  it('Test delete task', () => {
+    //create task
+    cy.intercept('POST', '**/tasks', backlogDataAddTask).as('create-issue');
+    cy.intercept('GET', '**/projects/*/backlogs', backlogDataAddTask).as('fetch-backlog-2');
+    cy.get('[data-testid="create-issue"]').click();
+    cy.get('[data-testid="create-issue-input"]').type('eat lunch');
+    cy.get('input').type('{enter}');
+    cy.wait('@create-issue');
+    cy.wait('@fetch-backlog-2');
+    cy.get('[data-testid="task-6350dbbca5c71eda4bcf78aa3"]').contains('eat lunch');
+    //delete task
+    cy.intercept('DELETE', '**/tasks/*', backlogData).as('delete-issue');
+    cy.intercept('GET', '**/projects/*/backlogs', backlogData).as('fetch-backlog');
+    cy.get('[data-testid="task-6350dbbca5c71eda4bcf78aa3"]').trigger('mouseover');
+    cy.get('[data-testid="hover-show-option-btn-6350dbbca5c71eda4bcf78aa3"]').click();
+    cy.get('[data-testid="delete-task-6350dbbca5c71eda4bcf78aa3"]').click();
+    cy.wait('@delete-issue');
+    cy.wait('@fetch-backlog');
+  });
 
-  // it('Test change title', () => {
-  //   cy.intercept('PUT', '**/tasks/*', backlogDataChangeTitle).as('change-title');
-  //   cy.intercept('GET', '**/projects/*/backlogs', backlogDataChangeTitle).as('fetch-backlog-3');
-  //   cy.get('[data-testid="task-hover-6350dbbca5c71eda4bcf78aa"]').trigger('mouseover');
-  //   cy.get('[data-testid="task-edit-btn-6350dbbca5c71eda4bcf78aa"]').click({ force: true });
-  //   cy.get('[data-testid="task-title-input-6350dbbca5c71eda4bcf78aa"]').clear().type('drink water');
-  //   cy.get('input').type('{enter}');
-  //   cy.wait('@change-title');
-  //   cy.wait('@fetch-backlog-3');
-  // });
+  it('Test change title', () => {
+    cy.intercept('PUT', '**/tasks/*', backlogDataChangeTitle).as('change-title');
+    cy.intercept('GET', '**/projects/*/backlogs', backlogDataChangeTitle).as('fetch-backlog-3');
+    cy.get('[data-testid="task-hover-6350dbbca5c71eda4bcf78aa"]').trigger('mouseover');
+    cy.get('[data-testid="task-edit-btn-6350dbbca5c71eda4bcf78aa"]').click({ force: true });
+    cy.get('[data-testid="task-title-input-6350dbbca5c71eda4bcf78aa"]').clear().type('drink water');
+    cy.get('input').type('{enter}');
+    cy.wait('@change-title');
+    cy.wait('@fetch-backlog-3');
+  });
   it('Test show priority', () => {
     cy.get('[data-testid="priority-btn-6350dbbca5c71eda4bcf78aa"]').should('exist');
     cy.get('[data-testid="priority-btn-6350dcfa560c73ef4f32e2a6"]').should('exist');
