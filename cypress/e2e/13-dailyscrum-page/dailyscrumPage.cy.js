abcafe7470e323d715325f66362fd6570f82757c
Summy <s4594789@student.uq.edu.au>
2022-11-14 22:57:36 +1000

    fix cypress test

--- cypress/e2e/13-dailyscrum-page/dailyscrumPage.cy.js
+++ cypress/e2e/13-dailyscrum-page/dailyscrumPage.cy.js
@@ -15,31 +15,37 @@ describe('Project page', () => {
   });
 
   it('Test should close dailyscrum page', () => {
-    cy.get('[data-testid="dailyscrum-btn"]').click();
+    cy.get('[data-testid="dailyscrum-btn"]').then((items) => {
+      items[0].click();
+    });
     cy.get('[data-testid="dailyscrum-close"]').click();
     cy.get('[data-testid="dailyscrum-header"]').should('not.exist');
   });
-  it('Test should cilck no and type reason', () => {
-    cy.intercept('GET', '**/dailyScrums/*/none/none', dailyScrum).as('fetch-dailyScrums');
-    cy.get('[data-testid="dailyscrum-btn"]').click();
+  it('Test should click no and type reason', () => {
+    cy.intercept('GET', '**/dailyScrums/*/*/*/*', dailyScrum).as('fetch-dailyScrums');
+    cy.get('[data-testid="dailyscrum-btn"]').then((items) => {
+      items[0].click();
+    });
     cy.wait('@fetch-dailyScrums');
     cy.get('[type="radio"]').check();
-    cy.get('[data-testid="dailyscrum-reason-636c674b38e168d571d8a619"]').type('I need support');
+    cy.get('[data-testid="dailyscrum-reason-6371d4176d614a9131b97e6f"]').type('I need support');
   });
   it('Test cancel should close dailyscrum page', () => {
-    cy.get('[data-testid="dailyscrum-btn"]').click();
+    cy.get('[data-testid="dailyscrum-btn"]').then((items) => {
+      items[0].click();
+    });
     cy.get('[data-testid="dailyscrum-cancel"]').click();
     cy.get('[data-testid="dailyscrum-header"]').should('not.exist');
   });
   it('Test should drag progress bar to 80%', () => {
-    cy.intercept('GET', '**/dailyScrums/*/none/14-11-2022/search-all', dailyScrum).as(
-      'fetch-dailyScrums'
-    );
+    cy.intercept('GET', '**/dailyScrums/*/*/*/*', dailyScrum).as('fetch-dailyScrums');
     const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
       window.HTMLInputElement.prototype,
       'value'
     ).set;
-    cy.get('[data-testid="dailyscrum-btn"]').click();
+    cy.get('[data-testid="dailyscrum-btn"]').then((items) => {
+      items[0].click();
+    });
     cy.wait('@fetch-dailyScrums');
     cy.get('[data-testid="dailyscrum-progress-bar-6371d4176d614a9131b97e6f"]').then(($range) => {
       const range = $range[0];
@@ -49,11 +55,11 @@ describe('Project page', () => {
     cy.get('[data-testid="dailyscrum-progress-6371d4176d614a9131b97e6f"]').contains('80%');
   });
   it('Test should submit data', () => {
-    cy.intercept('GET', '**/dailyScrums/*/none/14-11-2022/search-all', dailyScrum).as(
-      'fetch-dailyScrums'
-    );
-    cy.intercept('PATCH', '**/dailyScrums/*/*').as('update-dailyScrums');
-    cy.get('[data-testid="dailyscrum-btn"]').click();
+    cy.intercept('GET', '**/dailyScrums/*/*/*/*', dailyScrum).as('fetch-dailyScrums');
+    cy.intercept('PATCH', '**/dailyScrums/*').as('update-dailyScrums');
+    cy.get('[data-testid="dailyscrum-btn"]').then((items) => {
+      items[0].click();
+    });
     cy.wait('@fetch-dailyScrums');
     cy.get('[type="radio"]').check();
     cy.get('[data-testid="dailyscrum-reason-6371d4176d614a9131b97e6f"]').type('I need support');
