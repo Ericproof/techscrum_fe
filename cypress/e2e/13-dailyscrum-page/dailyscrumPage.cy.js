aaad063e29a4679f8fbe80f0ce2f191e55791305
Summy <s4594789@student.uq.edu.au>
2022-11-14 16:27:57 +1000

    fix cypress test

--- cypress/e2e/13-dailyscrum-page/dailyscrumPage.cy.js
+++ cypress/e2e/13-dailyscrum-page/dailyscrumPage.cy.js
@@ -20,11 +20,13 @@ describe('Project page', () => {
     cy.get('[data-testid="dailyscrum-header"]').should('not.exist');
   });
   it('Test should cilck no and type reason', () => {
-    cy.intercept('GET', '**/dailyScrums/*/none/none', dailyScrum).as('fetch-dailyScrums');
+    cy.intercept('GET', '**/dailyScrums/*/none/14-11-2022/search-all', dailyScrum).as(
+      'fetch-dailyScrums'
+    );
     cy.get('[data-testid="dailyscrum-btn"]').click();
     cy.wait('@fetch-dailyScrums');
     cy.get('[type="radio"]').check();
-    cy.get('[data-testid="dailyscrum-reason-636c674b38e168d571d8a619"]').type('I need support');
+    cy.get('[data-testid="dailyscrum-reason-6371d4176d614a9131b97e6f"]').type('I need support');
   });
   it('Test cancel should close dailyscrum page', () => {
     cy.get('[data-testid="dailyscrum-btn"]').click();
@@ -32,27 +34,31 @@ describe('Project page', () => {
     cy.get('[data-testid="dailyscrum-header"]').should('not.exist');
   });
   it('Test should drag progress bar to 80%', () => {
-    cy.intercept('GET', '**/dailyScrums/*/none/none', dailyScrum).as('fetch-dailyScrums');
+    cy.intercept('GET', '**/dailyScrums/*/none/14-11-2022/search-all', dailyScrum).as(
+      'fetch-dailyScrums'
+    );
     const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
       window.HTMLInputElement.prototype,
       'value'
     ).set;
     cy.get('[data-testid="dailyscrum-btn"]').click();
     cy.wait('@fetch-dailyScrums');
-    cy.get('[data-testid="dailyscrum-progress-bar-636c674b38e168d571d8a619"]').then(($range) => {
+    cy.get('[data-testid="dailyscrum-progress-bar-6371d4176d614a9131b97e6f"]').then(($range) => {
       const range = $range[0];
       nativeInputValueSetter.call(range, 80);
       range.dispatchEvent(new Event('change', { value: 80, bubbles: true }));
     });
-    cy.get('[data-testid="dailyscrum-progress-636c674b38e168d571d8a619"]').contains('80%');
+    cy.get('[data-testid="dailyscrum-progress-6371d4176d614a9131b97e6f"]').contains('80%');
   });
   it('Test should submit data', () => {
-    cy.intercept('GET', '**/dailyScrums/*/none/none', dailyScrum).as('fetch-dailyScrums');
+    cy.intercept('GET', '**/dailyScrums/*/none/14-11-2022/search-all', dailyScrum).as(
+      'fetch-dailyScrums'
+    );
     cy.intercept('PATCH', '**/dailyScrums/*/*').as('update-dailyScrums');
     cy.get('[data-testid="dailyscrum-btn"]').click();
     cy.wait('@fetch-dailyScrums');
     cy.get('[type="radio"]').check();
-    cy.get('[data-testid="dailyscrum-reason-636c674b38e168d571d8a619"]').type('I need support');
+    cy.get('[data-testid="dailyscrum-reason-6371d4176d614a9131b97e6f"]').type('I need support');
     cy.get('[data-testid="dailyscrum-submit"]').click();
     cy.wait('@update-dailyScrums');
   });
