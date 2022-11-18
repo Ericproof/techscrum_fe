f74b2d2d282a05077205775c1a9113c3b05050b3
Joe Zhou <zhouhaohangau@gmail.com>
2022-11-18 22:53:17 +1100

    removed unnecessary components

--- src/pages/AboutPageT2/AboutPageT2.tsx
+++ src/pages/AboutPageT2/AboutPageT2.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
-import Banner from './components/Banner/Banner';
 import styles from './AboutPageT2.module.scss';
-import CardsOfValues from './components/OurValuesCards/CardsOfValues';
-import AboutCareer from './components/AboutCareer/AboutCareer';
-import GetStarted from './components/GetStarted/GetStarted';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 export default function AboutPageT2() {
   return (
-    <div className={styles.container}>
-      <div className={styles.greeting}>
-        <Banner />
-      </div>
-      <div className="introduction-clip">
-        <img
-          src="https://embed-ssl.wistia.com/deliveries/c3648fe3ea850ffce14b1b3d78c6a517.webp?image_crop_resized=1920x1080"
-          alt="intro"
-          className={styles.introImg}
-        />
-      </div>
-      <div>
-        <CardsOfValues />
-      </div>
-      <div>
-        <AboutCareer />
-      </div>
-      <div className="background-white">
-        <GetStarted />
-      </div>
-    </div>
+    <>
+      <Header />
+      <section className={styles.container}>
+        <h1 className={styles.header}>People who make it happen</h1>
+        <hr className={styles.line} />
+      </section>
+      <Footer />
+    </>
   );
 }
