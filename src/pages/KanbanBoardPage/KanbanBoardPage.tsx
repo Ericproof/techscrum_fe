6b1d4ba6d0b8ad0871bd8510b7cf46b825ca0ccc
ZhouTian0716 <egglord0716@gmail.com>
2023-02-16 14:58:40 +1100

    feature sections ready

--- src/pages/KanbanBoardPage/KanbanBoardPage.tsx
+++ src/pages/KanbanBoardPage/KanbanBoardPage.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import { HiOutlineMailOpen } from 'react-icons/hi';
 import Header from '../../components/Header/Header';
 import shapeLight from '../../assets/shapeLight.png';
 import demoVideoOne from '../../assets/demoVideoOne.mp4';
 import darkCircle from '../../assets/shapeDark.png';
 import styles from './KanbanBoardPage.module.scss';
 import DemoVideo from '../../components/DemoVideo/DemoVideo';
+import FeatureSection from '../../components/FeatureSection/FeatureSection';
+
+const featureSectionPropsObjs = [
+  {
+    subTitle: 'flexible grouping',
+    title: 'Group your Boards your way.',
+    desc: 'Arrange your columns to analyze projects from any angle. Group by status, assignee, priority, and more.'
+  },
+  {
+    subTitle: 'everything view',
+    title: 'See all your Boards in one view.',
+    desc: 'Get an overview of where all your team projects stand at a glance with Everything view. See multiple workflows in one view, even if they have different statuses.'
+  },
+  {
+    subTitle: 'custom statuses',
+    title: 'Visualize any process with Custom Statuses.',
+    desc: 'Create unique statuses for any workflow, from sprints to multi-stage processes. Add new statuses or edit existing ones directly in Board view.'
+  },
+  {
+    subTitle: 'drag & drop',
+    title: 'Drag-and-drop your updates.',
+    desc: 'Quickly move tasks through workflows and adjust priorities. Drag and drop a task into any status and move the task up or down to change the priority of the task.'
+  },
+  {
+    subTitle: 'bulk action toolbar',
+    title: 'Make quick changes in bulk.',
+    desc: 'Save time making task updates in bulk with the Bulk Action Toolbar. Add assignees, change statuses, and delete tasks, all without leaving Board view.'
+  }
+];
 
 export default function KanbanBoardPage() {
   return (
-    <div>
+    <div className={styles.page}>
       <Header />
       <Fade>
         <div className={styles.unauthorizePageContainer}>
@@ -37,9 +67,23 @@ export default function KanbanBoardPage() {
                 Create the perfect Agile workflow with Board view.
               </h1>
               <p className={styles.text}>
-                Manage your project from start to finish with TechScrum.
+                Build a flexible Kanban system to visualize your work and improve project
+                management.
               </p>
-              <p className={styles.text}>No credit required.</p>
+              <form className={styles.flex_col}>
+                <div className={styles.email__wrapper}>
+                  <input
+                    type="email"
+                    className={styles.email__input}
+                    placeholder="Your work email"
+                    required
+                  />
+                  <HiOutlineMailOpen className={styles.email__icon} />
+                </div>
+                <button type="submit" className={styles.start__btn}>
+                  Get Started
+                </button>
+              </form>
             </div>
             <div className={styles.demo}>
               <DemoVideo src={demoVideoOne} />
@@ -47,6 +91,15 @@ export default function KanbanBoardPage() {
           </div>
         </div>
       </Fade>
+      {featureSectionPropsObjs.map((e, i) => (
+        <FeatureSection
+          key={e.desc}
+          subTitle={e.subTitle}
+          title={e.title}
+          desc={e.desc}
+          layout={i % 2 === 0 ? 'left' : 'right'}
+        />
+      ))}
     </div>
   );
 }
