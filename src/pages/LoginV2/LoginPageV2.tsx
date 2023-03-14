3f70a7715d1aabf193851fdb7f26b967d1bd1db8
Summy <s4594789@student.uq.edu.au>
2023-03-14 16:15:31 +1000

    file name change

--- src/pages/LoginV2/LoginPageV2.tsx
+++ src/pages/LoginV2/LoginPageV2.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import VerifyPageBackground from '../VerifyPage/VerifyPageBackground/VerifyPageBackground';
-import styles from './LoginPage.module.scss';
+import styles from './LoginPageV2.module.scss';
 import LoginMainV2 from './LoginMainV2/LoginMainV2';
 
 export default function LoginPageV2() {
