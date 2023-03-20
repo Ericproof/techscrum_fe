44de2ec7ef55113d5fd0364c2307a09a2b43f1b6
ZhouTian0716 <egglord0716@gmail.com>
2023-03-20 14:42:28 +1100

    backlog data type added

--- src/context/TasksByProjectProvider.tsx
+++ src/context/TasksByProjectProvider.tsx
@@ -1,18 +1,19 @@
 import React, { createContext, useEffect, useState, useMemo } from 'react';
 import { getTasksByProject } from '../api/task/task';
+import { IBacklogData } from '../types';
 
 const TasksByProjectContext = createContext<any>({});
 
 interface IProviderProps {
   projectId: string;
   children?: React.ReactNode;
-  backlogData?: any;
+  backlogData?: IBacklogData;
 }
 
 function TasksByProjectProvider({ projectId, backlogData, children }: IProviderProps) {
   const [tasks, setTasks] = useState<any>([]);
 
-  const a = useMemo(() => backlogData, [backlogData]);
+  const backlogDataMemo = useMemo(() => backlogData, [backlogData]);
 
   const fetchTasksByProject = async (id: string) => {
     const res = await getTasksByProject(id);
@@ -21,7 +22,7 @@ function TasksByProjectProvider({ projectId, backlogData, children }: IProviderP
 
   useEffect(() => {
     fetchTasksByProject(projectId);
-  }, [projectId, a]);
+  }, [projectId, backlogDataMemo]);
 
   return <TasksByProjectContext.Provider value={tasks}>{children}</TasksByProjectContext.Provider>;
 }
