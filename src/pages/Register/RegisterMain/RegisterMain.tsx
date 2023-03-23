ffa476c127539d9c93285f4818f1ba619b12870e
Summy <s4594789@student.uq.edu.au>
2023-03-24 07:34:08 +1000

    fix sign up url

--- src/pages/Register/RegisterMain/RegisterMain.tsx
+++ src/pages/Register/RegisterMain/RegisterMain.tsx
@@ -42,7 +42,7 @@ export default function RegisterMain() {
           setInvalidateStatus(true);
         }
       } else {
-        navigate('/register');
+        navigate('/v1/register');
       }
     };
     fetchEmailByToken();
