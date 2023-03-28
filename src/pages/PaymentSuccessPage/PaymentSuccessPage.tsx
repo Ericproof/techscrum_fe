dc1735aab2227850afe7a5cc9a6cd8e0065b21fc
ZhouTian0716 <egglord0716@gmail.com>
2023-03-28 14:33:15 +1100

    updated as per review

--- src/pages/PaymentSuccessPage/PaymentSuccessPage.tsx
+++ src/pages/PaymentSuccessPage/PaymentSuccessPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import MainMenuV2 from '../MainMenuV2/MainMenuV2';
-import SubSettingMenu from '../../lib/SubSettingMenu/SubSettingMenu';
 import styles from './PaymentSuccessPage.module.scss';
 import paymentSuccess from '../../assets/payment-success.webp';
 
@@ -24,30 +22,26 @@ const formatTimeStamp = (stamp: number): string => {
 
 export default function PaymentSuccessPage() {
   return (
-    <div className={styles.pageContainer}>
-      <MainMenuV2 />
-      <SubSettingMenu />
-      <div className={styles.sectionContainer}>
-        <h2>Payment Success</h2>
-        <div className={styles.contents}>
-          <img src={paymentSuccess} alt="paymentSuccess" className={styles.paymentImg} />
-          <p className={styles.textSecondary}>Hi, admin {user.name}</p>
-          <p>
-            Welcome to your {user.period} days free trial of {user.plan} subscription! The payment
-            method you provided will be charged monthly/yearly starting{' '}
-            {formatTimeStamp(user.startDate)}.
-          </p>
-          <p>
-            As a member, you can explore, manage, and cancel your subscription at any time by
-            visiting plan & billing settings.
-          </p>
-          <p>Welcome aboard!</p>
-          <p className={styles.textSecondary}>The TechScrum Team</p>
-          <Link className={styles.linkBtn} to="/projects">
-            Go Projects
-          </Link>
-        </div>
+    <div className={styles.sectionContainer}>
+      <h2>Payment Success</h2>
+      <img src={paymentSuccess} alt="paymentSuccess" className={styles.paymentImg} />
+      <div className={styles.contents}>
+        <p className={styles.textSecondary}>Hi, admin {user.name}</p>
+        <p>
+          Welcome to your {user.period} days free trial of {user.plan} subscription! The payment
+          method you provided will be charged monthly/yearly starting{' '}
+          {formatTimeStamp(user.startDate)}.
+        </p>
+        <p>
+          As a member, you can explore, manage, and cancel your subscription at any time by visiting
+          plan & billing settings.
+        </p>
+        <p>Welcome aboard!</p>
+        <p className={styles.textSecondary}>The TechScrum Team</p>
       </div>
+      <Link className={styles.linkBtn} to="/projects">
+        Go Projects
+      </Link>
     </div>
   );
 }
