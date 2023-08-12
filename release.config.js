ed14931c99dde4af774a6cdaeabfc869601aefab
kitman <kitman200220022002@gmail.com>
2023-08-13 07:25:19 +1000

    testing

--- release.config.js
+++ release.config.js
@@ -5,6 +5,13 @@ const config = {
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       '@semantic-release/changelog',
+      [
+        "@semantic-release/npm",
+        {
+          "npmPublish": false,
+          "tarballDir": false,
+        }
+      ],
       ['@semantic-release/git', {
         'assets': ['dist/*.js', 'dist/*.js.map'],
         'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
