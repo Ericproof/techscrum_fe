d7b0adde09e3db82e51ec2d8fad397a5e06ecef9
kitman <kitman200220022002@gmail.com>
2023-08-16 07:32:18 +1000

    feat: added deprecation HOC

--- src/pages/Register/RegisterPage.tsx
+++ src/pages/Register/RegisterPage.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import VerifyPageBackground from '../VerifyPage/VerifyPageBackground/VerifyPageBackground';
 import styles from './RegisterPage.module.scss';
 import RegisterMain from './RegisterMain/RegisterMain';
+import Deprecated from '../../components/DEPRECATED/Deprecated';
 
 export default function RegistePager() {
   return (
-    <div className={styles.registerContainer}>
-      <VerifyPageBackground>
-        <RegisterMain />
-      </VerifyPageBackground>
-    </div>
+    <Deprecated>
+      <div className={styles.registerContainer}>
+        <VerifyPageBackground>
+          <RegisterMain />
+        </VerifyPageBackground>
+      </div>
+    </Deprecated>
   );
 }
